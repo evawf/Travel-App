9254c40fe2620ec4a8b14c1f0c76a2ffe28df951
eva.wang <eva.wang81@gmail.com>
2021-04-11 20:04:03 +0800

    Update inputCherker

--- src/client/js/inputChecker.js
+++ src/client/js/inputChecker.js
@@ -1,9 +1,6 @@
 let checkForInput = (destination, departureDate) => {
     if(destination !== "" && departureDate !== ""){
-        const ToDate = new Date();
-        return (new Date(departureDate).getTime() >= ToDate.getTime());
-    } else {
-        return false;
+        return true;
     }
 }
 
