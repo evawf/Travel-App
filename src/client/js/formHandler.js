62fa9782006cdf1d94c424abfce174ff461b5f7f
eva.wang <eva.wang81@gmail.com>
2021-04-14 14:39:08 +0800

    Ready to setup local storage

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -47,7 +47,7 @@ async function handleSubmit(event) {
         // showPreLoader();
         const data = await postData('http://localhost:8081/destination', { destination, departureDate });
         console.log(data);
-        displayTrips(data);
+        // displayTrips(data);
     } else {
         errorMsg.innerHTML = "Please enter both destination and departure date!"
         errorMsg.style.color = "red";
@@ -57,74 +57,74 @@ async function handleSubmit(event) {
 
 
 //Display Trip Planning
-const displayTrips = (data) => {
-    //Show recent trip input
-    const recentTripInfo = data[data.length-1];
-    console.log(recentTripInfo);
-    document.getElementById('location').innerHTML = recentTripInfo.destination;
-    document.getElementById('date').innerHTML = "Departure date: " + recentTripInfo.departureDate;
-    const weatherForecast = recentTripInfo.forecast;
-    for (let i = 0; i < weatherForecast.length; i++) {
-        if(weatherForecast[i].date == recentTripInfo.departureDate){
-            document.getElementById('temp').innerHTML = weatherForecast[i].temp + "&deg;C";
-            document.getElementById('weather').innerHTML = weatherForecast[i].weather.description;
-        }
-    }
+// const displayTrips = (data) => {
+//     //Show recent trip input
+//     const recentTripInfo = data[data.length-1];
+//     console.log(recentTripInfo);
+//     document.getElementById('location').innerHTML = recentTripInfo.destination;
+//     document.getElementById('date').innerHTML = "Departure date: " + recentTripInfo.departureDate;
+//     const weatherForecast = recentTripInfo.forecast;
+//     for (let i = 0; i < weatherForecast.length; i++) {
+//         if(weatherForecast[i].date == recentTripInfo.departureDate){
+//             document.getElementById('temp').innerHTML = weatherForecast[i].temp + "&deg;C";
+//             document.getElementById('weather').innerHTML = weatherForecast[i].weather.description;
+//         }
+//     }
 
-    //SlideShow for destination photos
-    const photos = recentTripInfo.photos;
-    const slides_container = document.getElementById('slides_container');
-    for (let i = 0; i < photos.length; i++) {
-        const img_container = document.createElement('div');
-        const img = document.createElement('img');
-        img_container.classList.add('img_container');
-        img.src = photos[i];
-        img_container.appendChild(img);
-        slides_container.appendChild(img_container);
-    }
+//     //SlideShow for destination photos
+//     const photos = recentTripInfo.photos;
+//     const slides_container = document.getElementById('slides_container');
+//     for (let i = 0; i < photos.length; i++) {
+//         const img_container = document.createElement('div');
+//         const img = document.createElement('img');
+//         img_container.classList.add('img_container');
+//         img.src = photos[i];
+//         img_container.appendChild(img);
+//         slides_container.appendChild(img_container);
+//     }
 
-    let slideIndex = 1;
-    showSlides(slideIndex);
+//     let slideIndex = 1;
+//     showSlides(slideIndex);
 
-    window.plusSlides = (n) => {
-        showSlides(slideIndex += n);
-    }
+//     window.plusSlides = (n) => {
+//         showSlides(slideIndex += n);
+//     }
 
-    function showSlides(n) {
-        let i;
-        const img_containers = document.getElementsByClassName('img_container');
-        if (n > img_containers.length) { slideIndex = 1 }
-        if (n < 1) { slideIndex = img_containers.length }
-        for (i = 0; i < img_containers.length; i++) {
-            img_containers[i].style.display = "none";
-        }
-        img_containers[slideIndex-1].style.display = "block";
-    }
-}
+//     function showSlides(n) {
+//         let i;
+//         const img_containers = document.getElementsByClassName('img_container');
+//         if (n > img_containers.length) { slideIndex = 1 }
+//         if (n < 1) { slideIndex = img_containers.length }
+//         for (i = 0; i < img_containers.length; i++) {
+//             img_containers[i].style.display = "none";
+//         }
+//         img_containers[slideIndex-1].style.display = "block";
+//     }
+// }
 
 
 
-//List future trip
-document.getElementById('saveTrip').addEventListener('click', saveTrip);
-async function saveTrip() {
-    const request = await fetch('http://localhost:8081/trips');
-    try{
-        const allData = await request.json();
-        console.log(allData);
-        displayIncomingTrip(allData);
-    }
-    catch(error) {
-        console.log('error', error);
-    }
-}
+// //List future trip
+// document.getElementById('saveTrip').addEventListener('click', saveTrip);
+// async function saveTrip() {
+//     const request = await fetch('http://localhost:8081/trips');
+//     try{
+//         const allData = await request.json();
+//         console.log(allData);
+//         displayIncomingTrip(allData);
+//     }
+//     catch(error) {
+//         console.log('error', error);
+//     }
+// }
 
-function displayIncomingTrip(allData) {
-    const tripCard = document.createElement('div');
-    const trip_destination = document.createElement('div');
-    const trip_date = document.createElement('div');
-    const trip_weather = document.createElement('div');
-    const trip_temp = document
-}
+// function displayIncomingTrip(allData) {
+//     const tripCard = document.createElement('div');
+//     const trip_destination = document.createElement('div');
+//     const trip_date = document.createElement('div');
+//     const trip_weather = document.createElement('div');
+//     const trip_temp = document
+// }
 
 //List past trip
 
